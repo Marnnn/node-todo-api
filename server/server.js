38064573dcc49841345690b57740ec428baaf055
Graham Fletcher <marnburger@gmail.com>
2017-01-03 23:09:05 +0700

    Added allow origin for local host

--- server/server.js
+++ server/server.js
@@ -33,8 +33,8 @@ app.get('/todos', authenticate, (req, res) => {
     Todo.find({
         _creator: req.user._id
     }).then((todos) => {
-        res.header('Access-Control-Allow-Origin', 'http://localhost/3000')
-            .send({todos});
+        req.header('Access-Control-Allow-Origin', 'http://localhost/3000');
+        res.send({todos});
     }, (e) => {
         res.status(400).send(e);
     });
